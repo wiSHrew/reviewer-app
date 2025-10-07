c9394b89d2ac22da097a03c1b6c01f95941910a6
Jerome Desear <219304646+JeromeDesear@users.noreply.github.com>
2025-10-07 16:39:04 +0800

    v1 done

--- main.js
+++ main.js
@@ -20,13 +20,14 @@ function processFile() {
     
     // split questions, choices and answers
     answersString = questionsString.split('AnswerKey')[1];
-    questions = questionsString.split('AnswerKey')[1].split('\n');
+    questions = questionsString.split('AnswerKey')[0].split('\n');
     answers = answersString.split('\n');
 
     let allysa = [];
     // clean empty lines
     for (let i = 0; i < questions.length; i++) {
         if (questions[i].trim() === "") continue;
+        if (questions[i].trim().includes("Question")) continue;
         allysa.push(questions[i]);
     }
     questions = allysa;
@@ -34,15 +35,25 @@ function processFile() {
 
     // separate choices
     for (let i = 0; i < questions.length; i++) {
-        if (questions[i].includes('A.') || questions[i].includes('B.') || questions[i].includes('C.') || questions[i].includes('D.')) {
+        if (questions[i].includes('A. ') || questions[i].includes('B. ') || questions[i].includes('C. ') || questions[i].includes('D. ')) {
             choices.push(questions[i]);
             allysa.push(i);
         }
     }
+    allysa.sort((a, b) => b - a);
     // remove choices from questions
     for (let i = 0; i < allysa.length; i++) {
         questions.splice(allysa[i], 1);
     }
+    allysa = [];
+
+    // group the choices by 4
+    for (let i = 0; i < choices.length; i += 4) {
+        let group = choices.slice(i, i + 4);
+        allysa.push(group);
+    }
+    choices = allysa;
+    allysa = [];
 
     // clean empty lines
     for (let i = 0; i < answers.length; i++) {
@@ -53,17 +64,76 @@ function processFile() {
     allysa = [];
 
     // start the review
-    const button = document.getElementById("next");
-    button.hidden = false;
-    index = 0;
-    console.log(questions[0]);
-    // displayQuestions()
+    document.getElementById("choicesForm").hidden = false;
+    document.getElementById("next").hidden = false;
+    nextQuestion(index);
+
+    console.log(questions);
+    console.log(choices);
+    console.log(answers);
   };
 
   reader.readAsText(file);
 }
 
-function displayQuestion() {
-  const output = document.getElementById('output');
-  output.textContent = questions[index] + "\n";
+function nextQuestion() {
+    index++;
+    if (index < questions.length) {
+        displayQuestion(index);
+    } else {
+        document.getElementById("output").textContent = "End of questions! \\(0^◇^0)/";
+        document.getElementById("choicesForm").hidden = true;
+        document.getElementById("next").hidden = true;
+        document.getElementById("answer").hidden = true;
+    }
+}
+
+function displayQuestion(index) {
+    // reset form
+    unlockRadios();
+
+    // display question
+    const output = document.getElementById('output');
+    output.textContent = questions[index] + "\n";
+
+    // display choices
+    const letters = ['a', 'b', 'c', 'd'];
+    for (let i = 0; i < 4; i++) {
+        let letter = letters[i];
+        const labelElement = document.querySelector(`label[for="${letter}"]`);
+        labelElement.textContent = choices[index][i];
+    }
+
+    // wait for user input and check answer
+    document.getElementById('choicesForm').addEventListener('change', function(event) {
+        if (event.target.name === 'choice') {
+            const selectedValue = event.target.value;
+            console.log('You selected:', selectedValue);
+            let letterAns = answers[index].split('.')[0].toLowerCase();
+            let answer = answers[index].split(letterAns);
+            if (selectedValue === letterAns.trim()) {
+                answer = "Correct! o(〃＾▽＾〃)o\n" + answer;
+            }
+            else {
+                answer = "Wrong! (╥﹏╥)\n" + answer;
+            }
+            document.getElementById("answer").textContent = answer;
+            lockRadios();
+        }
+    });
+}
+
+
+function lockRadios() {
+    const radios = document.querySelectorAll('input[name="choice"]');
+    radios.forEach(r => r.disabled = true);
+    document.getElementById("next").disabled = false;
+}
+
+function unlockRadios() {
+    const radios = document.querySelectorAll('input[name="choice"]');
+    radios.forEach(r => r.disabled = false);
+    radios.forEach(r => r.checked = false);
+    document.getElementById("next").disabled = true;
+    document.getElementById("answer").textContent = "";
 }
